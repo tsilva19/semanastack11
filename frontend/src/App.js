dc91b1cc93a17ad018f8cb7056395bb32a049e18
thialvs <thiagoalvessilva@correio.itau.com.br>
2020-03-28 08:12:09 -0300

    Front-End do App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './Header';
+import  './global.css';
+import Routes from './routes';
+
 
 function App() {
+  // const [contador, setContador] = useState(0);
+
+  // function increment(){
+  //   setContador(contador + 1)
+  //   console.log(contador);
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes/>
+    // <div>
+    //   <Header>
+    //   Contador: {contador}
+    // </Header>
+    // <button onClick={increment}>Incrementar</button>
+    // </div>
+    
+   );
 }
 
 export default App;
---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,81 @@
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi';
+
+
+
+import './styles.css';
+
+
+import logo from '../../assets/logo.svg'
+import api from '../../services/api';
+
+export default function NewIncident(){
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const history = useHistory();
+
+    const ongId = localStorage.getItem('ongId');
+
+    async function handleNewIncident(e){
+        e.preventDefault();
+
+        const  data = {
+            title,
+            description,
+            value,
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId
+                }
+            })
+
+            history.push('/profile');
+        }catch (err) {
+            alert('Erro ao cadastrar caso, tente novamente.');
+        }
+
+    }
+
+    return(
+        <div className="newincident-container">
+            <div className="content">
+                <section>
+                    <img src={logo} alt="logo do site"/>
+
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso </p>
+                    <Link className="back-link" to="/profile">
+                      <FiArrowLeft size={16} color="orangered"/>
+                       Voltar para o Home
+                    </Link>
+                
+                </section>
+                <form onSubmit={handleNewIncident}>
+                    <input 
+                        placeholder= "Titulo do caso"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea 
+                        placeholder="Descrição"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />   
+                    <input 
+                        placeholder="Valor em Reais"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
+                    
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    )
+}
